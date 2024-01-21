3c1de6b93bc1d3d914994a3d5a37689dabece1be
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-01-21 17:18:53 -0500

    Delete Tribal_Pelts_Mushroom_Enhanced directory

--- Tribal_Pelts_Mushroom_Enhanced/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TribalPeltsMushroom";
-        public const string PLUGIN_NAME = "TribalPeltsMushroom";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
